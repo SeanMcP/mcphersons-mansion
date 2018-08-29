0a098aa605560de39044c338854920788dec2665
Sean McPherson <seanmcp25@gmail.com>
2018-08-29 12:39:48 -0400

    Read JSON data in Room class

--- classes/Room.rb
+++ classes/Room.rb
@@ -1,18 +1,26 @@
+require 'json'
+raw = File.read('./data/desc.json')
+$data = JSON.parse(raw)
+
 class Room
-    def initialize( type, item )
+    def initialize( type, items )
         @type = type
-        @item = item
+        @items = items
     end
 
     def look
-        puts "Looks like a %s." % [ @type ]
+        puts $data['room'][@type]
+        puts "Around the #{@type}, you see:"
+        for item in @items
+            puts item
+        end
     end
 
     def search
-        puts "You found a %s!" % [ @item ]
+        puts "You found a #{@type}!"
     end
 end
 
-my_room = Room.new "bedroom", "key"
+my_room = Room.new "bedroom", [ "desk", "bookshelf" ]
 my_room.look()
-my_room.search()
+# my_room.search()
--- index.rb
+++ index.rb
@@ -2,4 +2,4 @@ require_relative 'utils/input'
 
 player_name = inputs("What's your name?")
 
-puts "Welcome, %s, to McPherson's Mansion!" % [player_name]
+puts "Welcome, #{player_name}, to McPherson's Mansion!"
